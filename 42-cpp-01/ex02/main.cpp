d3ca04a97eeb0379281e002d63c47c2b0d88545f
Lorenzo Yanga <lorenzoyanga@gmail.com>
2026-04-06 21:06:43 +0800

    didnt read subject pdf...

--- 42-cpp-01/ex02/main.cpp
+++ 42-cpp-01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 06:49:37 by lyanga            #+#    #+#             */
-/*   Updated: 2026/04/06 19:24:31 by lyanga           ###   ########.fr       */
+/*   Updated: 2026/04/06 21:06:29 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int main(void)
 {
-	std::string str = "THE STRING";
+	std::string str = "HI THIS IS BRAIN";
 	std::string* stringPTR = &str;
 	std::string& stringREF = str;
 
