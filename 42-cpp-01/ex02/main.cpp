7ca5ef2b7a41deefeb8c48a1d4f7d8fe226c8040
Lorenzo Yanga <lorenzoyanga@gmail.com>
2026-04-06 16:58:02 +0800

    01-02 cleanup

--- 42-cpp-01/ex02/main.cpp
+++ 42-cpp-01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 06:49:37 by lyanga            #+#    #+#             */
-/*   Updated: 2026/03/24 06:51:23 by lyanga           ###   ########.fr       */
+/*   Updated: 2026/04/06 16:54:07 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 #include <string>
 
 int main(void) {
-    std::string str = "HI THIS IS BRAIN";
+    std::string str = "THE STRING";
     std::string* stringPTR = &str;
     std::string& stringREF = str;
 
     std::cout << "Memory address of the string variable: " << &str << std::endl;
-    std::cout << "Memory address held by stringPTR:    " << stringPTR << std::endl;
-    std::cout << "Memory address held by stringREF:    " << &stringREF << std::endl;
+    std::cout << "Memory address held by stringPTR:      " << stringPTR << std::endl;
+    std::cout << "Memory address held by stringREF:      " << &stringREF << std::endl;
 
     std::cout << std::endl;
 
     std::cout << "Value of the string variable:          " << str << std::endl;
-    std::cout << "Value pointed to by stringPTR:       " << *stringPTR << std::endl;
-    std::cout << "Value pointed to by stringREF:       " << stringREF << std::endl;
+    std::cout << "Value pointed to by stringPTR:         " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF:         " << stringREF << std::endl;
 
     return 0;
 }
