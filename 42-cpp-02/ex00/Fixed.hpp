1ac19d4ffdb5cb6a6854f336736d40a8f6b0564e
Lorenzo Yanga <lorenzoyanga@gmail.com>
2026-04-07 13:58:03 +0800

    02-00 cleanup

--- 42-cpp-02/ex00/Fixed.hpp
+++ 42-cpp-02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 06:53:48 by lyanga            #+#    #+#             */
-/*   Updated: 2026/04/06 19:24:33 by lyanga           ###   ########.fr       */
+/*   Updated: 2026/04/07 13:57:41 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@
 # include <iostream>
 
 class Fixed {
-public:
-	Fixed(void);
-	Fixed(const Fixed& other);
-	Fixed& operator=(const Fixed& other);
-	~Fixed(void);
+	public:
+		Fixed(void);
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed(void);
 
-	int getRawBits(void) const;
-	void setRawBits(int const raw);
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
 
-private:
-	int _fixedPointValue;
-	static const int _fractionalBits;
+	private:
+		int _fixedPointValue;
+		static const int _fractionalBits;
 };
 
 #endif
