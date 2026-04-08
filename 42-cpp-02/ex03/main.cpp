86b73efe17a41e31ea56a534836fb489730c6332
Lorenzo Yanga <lorenzoyanga@gmail.com>
2026-04-08 19:45:39 +0800

    typo.

--- 42-cpp-02/ex03/main.cpp
+++ 42-cpp-02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 06:55:14 by lyanga            #+#    #+#             */
-/*   Updated: 2026/04/07 16:06:01 by lyanga           ###   ########.fr       */
+/*   Updated: 2026/04/08 19:45:36 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int main(void) {
 
 	std::cout << "Point p1 is " << (bsp(a, b, c, p1) ? "inside" : "outside") << std::endl;
 	std::cout << "Point p2 is " << (bsp(a, b, c, p2) ? "inside" : "outside") << std::endl;
-	std::cout << "Point p3 is " << (bsp(a, b, c, p3) ? "outside" : "outside") << std::endl;
+	std::cout << "Point p3 is " << (bsp(a, b, c, p3) ? "inside" : "outside") << std::endl;
 	std::cout << "Point p4 is " << (bsp(a, b, c, p4) ? "inside" : "outside") << std::endl;
 	std::cout << "Point p5 is " << (bsp(a, b, c, p5) ? "inside" : "outside") << std::endl;
 
