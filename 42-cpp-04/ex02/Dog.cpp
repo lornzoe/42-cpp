022721c66f649b2366aa44a877ebb7ec69649075
Lorenzo Yanga <lorenzoyanga@gmail.com>
2026-04-11 04:48:50 +0800

    changed msg

--- 42-cpp-04/ex02/Dog.cpp
+++ 42-cpp-04/ex02/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 07:03:36 by lyanga            #+#    #+#             */
-/*   Updated: 2026/04/06 19:24:43 by lyanga           ###   ########.fr       */
+/*   Updated: 2026/04/11 04:48:31 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ Dog::~Dog()
 
 void Dog::makeSound() const
 {
-	std::cout << "Woof! Woof!" << std::endl;
+	std::cout << "Woof." << std::endl;
 }
 
 Brain *Dog::getBrain() const
