6437ea1828473edb74ee325c4c4b47c658ade9be
Lorenzo Yanga <lorenzoyanga@gmail.com>
2026-04-11 01:24:50 +0800

    Makefile update to use CXX for cpp 04

--- 42-cpp-04/ex00/WrongCat.hpp
+++ 42-cpp-04/ex00/WrongCat.hpp
@@ -6,7 +6,7 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 07:02:39 by lyanga            #+#    #+#             */
-/*   Updated: 2026/04/06 19:24:40 by lyanga           ###   ########.fr       */
+/*   Updated: 2026/04/11 01:24:50 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@
 
 class WrongCat : public WrongAnimal
 {
-public:
-	WrongCat();
-	WrongCat(const WrongCat &other);
-	WrongCat &operator=(const WrongCat &other);
-	~WrongCat();
+	public:
+		WrongCat();
+		WrongCat(const WrongCat &other);
+		WrongCat &operator=(const WrongCat &other);
+		~WrongCat();
 
-	void makeSound() const;
+		void makeSound() const;
 };
 
 #endif
